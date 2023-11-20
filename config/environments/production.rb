8a12aba7c39d89a242e27f64ae740a6c8d36227d
Alexis Dutoya <alexis.dutoya@gmail.com>
2023-11-20 16:11:33 +0100

    add devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Default url options
+  config.action_mailer.default_url_options = { host: 'https://louetonchelou-danarasta12-eeda60f7d021.herokuapp.com/' }
 end
