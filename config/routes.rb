f829a9d4db162fff27f542092ceb7e3b1b6fb13e
Pierre Eloy <pierre.eloy12@gmail.com>
2023-11-21 15:26:58 +0100

    MEGA CHANGEMENT UN PEU DE TOUT

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :talents, except: %i[new] do
+  resources :talents do
     resources :bookings, only: %i[new create]
     resources :reviews, only: %i[create]
   end
   resources :bookings, only: %i[destroy]
+  get "dashboard", to: "pages#dashboard"
 end
