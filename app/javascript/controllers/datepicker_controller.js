fce8c6e131b2607fb333c370a4251fc5e54967cc
Clement Nicolas <clementnicolasprobis@gmail.com>
2023-11-22 12:31:35 +0100

    calendrier et redirect done

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,28 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr"
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.element);
   }
 }
-
-// import flatpickr from "flatpickr"
-// import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
-
-
-// const initFlatpickr = () => {
-//   const bookingForm = document.getElementById('booking-form-div');
-//   if (bookingForm) {
-//     const bookings = JSON.parse(bookingForm.dataset.bookings);
-//     flatpickr("#range_start", {
-//       plugins: [new rangePlugin({ input: "#range_end"})],
-//       minDate: "today",
-//       inline: true,
-//       dateFormat: "Y-m-d",
-//       "disable": bookings,
-//     })
-//   }
-// };
-
-// export { initFlatpickr };
