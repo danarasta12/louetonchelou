792b545b5ce4accc65ea95119b8a404928906013
Pierre Eloy <pierre.eloy12@gmail.com>
2023-11-22 11:44:52 +0100

    SEED GOOD

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -1,5 +1,5 @@
 class TalentsController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: %i[index show]
   before_action :set_talent, only: %i[create show edit update]
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_120137) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_104314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,7 +71,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_120137) do
     t.string "talent_type"
     t.float "price"
     t.string "performance_duration"
-    t.string "medias"
     t.text "description"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_talents_on_user_id"
