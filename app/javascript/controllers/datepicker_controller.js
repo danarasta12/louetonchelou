5986192d97103eed742ff2c07e3b27245dd1b174
Clement Nicolas <clementnicolasprobis@gmail.com>
2023-11-22 11:26:28 +0100

    small changes on flatpickr

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
+    flatpickr(this.element)
   }
 }
