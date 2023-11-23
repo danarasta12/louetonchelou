1acea9181884e02a73bc4f1951829fe45276a538
Clement Nicolas <clementnicolasprobis@gmail.com>
2023-11-23 14:07:20 +0100

    update calendrier show

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings
   has_many :talents, dependent: :destroy
+  has_one_attached :profile_picture
 
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
